474efad839c26e14b9cb480e70977fd8a9bffa12
zsx <18273117003@163.com>
2019-11-05 20:20:11 +0800

    multiple graphql schema deal

---
+++ graphql-with-nodejs/jsGraphQLRead.md
@@ -0,0 +1,14 @@
+# 安装nodejs
+下载地址：https://nodejs.org/en/download/current/
+# 创建目录graphql-with-nodejs
+cd graphql-with-nodejs
+# 初始化npm
+npm init -y
+# 安装express
+npm install express
+# 安装express-graphql
+npm install express-graphql graphql
+# 安装lodash
+npm install lodash --save
+# 执行命令
+node server.js
--- src/main/java/com/zsx/graphql/GraphQLProvider.java
+++ src/main/java/com/zsx/graphql/GraphQLProvider.java
@@ -1,66 +1,72 @@
-//package com.zsx.graphql;
-//
-//import com.google.common.base.Charsets;
-//import com.google.common.io.Resources;
-//import graphql.ExecutionResult;
-//import graphql.GraphQL;
-//import graphql.schema.GraphQLSchema;
-//import graphql.schema.idl.RuntimeWiring;
-//import graphql.schema.idl.SchemaGenerator;
-//import graphql.schema.idl.SchemaParser;
-//import graphql.schema.idl.TypeDefinitionRegistry;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.stereotype.Component;
-//
-//import javax.annotation.PostConstruct;
-//import java.io.IOException;
-//import java.net.URL;
-//
-//import static graphql.schema.idl.TypeRuntimeWiring.newTypeWiring;
-//
-//@Component
-//public class GraphQLProvider {
-//
-//    private GraphQL graphQL;
-//
-//    @Autowired
-//    private GraphQLDataFetchers graphQLDataFetchers;
-//
-//    @Bean
-//    public GraphQL graphQL() {
-//        return graphQL;
-//    }
-//
-//    @PostConstruct
-//    public void init() throws IOException {
-//        URL url = Resources.getResource("schema.graphqls");
-//        String sdl = Resources.toString(url, Charsets.UTF_8);
-//        GraphQLSchema graphQLSchema = buildSchema(sdl);
-//        this.graphQL = GraphQL.newGraphQL(graphQLSchema).build();
-//        String query = "{bookById(id: \"book-1\") {id, name, pageCount, author {firstName, lastName}}}";
-//        ExecutionResult executionResult = graphQL.execute(query);
-//        System.out.println("==========" + executionResult.toSpecification());
-//    }
-//
-//    private GraphQLSchema buildSchema(String sdl) {
-//        // TODO: we will create the schema here later
-//        TypeDefinitionRegistry typeRegistry = new SchemaParser().parse(sdl);
-//        RuntimeWiring runtimeWiring = buildWiring();
-//        SchemaGenerator schemaGenerator = new SchemaGenerator();
-//        return schemaGenerator.makeExecutableSchema(typeRegistry, runtimeWiring);
-//    }
-//
-//
-//    private RuntimeWiring buildWiring() {
-//        return RuntimeWiring.newRuntimeWiring()
-//                .type(newTypeWiring("Query")
-//                        .dataFetcher("bookById", graphQLDataFetchers.getBookByIdDataFetcher()))
-//                .type(newTypeWiring("Book")
-//                        .dataFetcher("author", graphQLDataFetchers.getAuthorDataFetcher())
-//                        // This line is new: we need to register the additional DataFetcher
-//                        .dataFetcher("pageCount", graphQLDataFetchers.getPageCountDataFetcher()))
-//                .build();
-//    }
-//
-//}
+package com.zsx.graphql;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.net.URL;
+
+import static graphql.schema.idl.TypeRuntimeWiring.newTypeWiring;
+
+@Component
+public class GraphQLProvider {
+
+    private GraphQL graphQL;
+
+    @Autowired
+    private BookGraphQLDataFetchers bookGraphQLDataFetchers;
+    @Autowired
+    private CompanyGraphQLDataFetchers companyGraphQLDataFetchers;
+
+    @Bean
+    public GraphQL graphQL() {
+        return graphQL;
+    }
+
+    @PostConstruct
+    public void init() throws IOException {
+        URL urlSchema = Resources.getResource("schema.graphqls");
+        URL urlCompany = Resources.getResource("company.graphqls");
+        String sdlSchema = Resources.toString(urlSchema, Charsets.UTF_8);
+        String sdlCompany = Resources.toString(urlCompany, Charsets.UTF_8);
+        this.graphQL = GraphQL.newGraphQL(buildSchema(sdlSchema, sdlCompany)).build();
+    }
+
+    private GraphQLSchema buildSchema(String ... schemaInputs) {
+        // TODO: we will create the schema here later
+        TypeDefinitionRegistry typeRegistry = new TypeDefinitionRegistry();
+        // each registry is merged into the main registry
+        SchemaParser schemaParser = new SchemaParser();
+        for (String schemaInput : schemaInputs) {
+            typeRegistry.merge(schemaParser.parse(schemaInput));
+        }
+        RuntimeWiring runtimeWiring = buildWiring();
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+        return schemaGenerator.makeExecutableSchema(typeRegistry, runtimeWiring);
+    }
+
+
+    private RuntimeWiring buildWiring() {
+        return RuntimeWiring.newRuntimeWiring()
+                .type(newTypeWiring("Query")
+                        .dataFetcher("bookById", bookGraphQLDataFetchers.getBookByIdDataFetcher()))
+                .type(newTypeWiring("Book")
+                        .dataFetcher("author", bookGraphQLDataFetchers.getAuthorDataFetcher())
+                        // This line is new: we need to register the additional DataFetcher
+                        .dataFetcher("pageCount", bookGraphQLDataFetchers.getPageCountDataFetcher()))
+                .type(newTypeWiring("Query")
+                        .dataFetcher("companyById", companyGraphQLDataFetchers.getCompanyByIdDataFetcher()))
+                .build();
+    }
+
+}
